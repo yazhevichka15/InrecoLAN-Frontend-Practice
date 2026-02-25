0cb3489171c8c23682d64586ec7a0660a395bbe1
DmitriyPodbornov <dmitriypodbornov37@gmail.com>
2026-02-25 17:21:00 +0300

    удалил лишние экспорты в index'е

--- src/widgets/footer/index.ts
+++ src/widgets/footer/index.ts
@@ -1,5 +1,1 @@
 export { Footer } from './Footer';
-export { FooterAbout } from './FooterAbout';
-export { FooterAdress } from './FooterAdress';
-export { FooterCatalog } from './FooterCatalog';
-export { FooterContacts } from './FooterContacts';
