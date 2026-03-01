b7e7286480600fd0ad976671843d27a63a431575
IrinaD7 <irinadimakova05@yandex.ru>
2026-03-02 00:09:41 +0300

    feat: реализована  кнопка добавления в избранное

--- src/shared/ui/ProductCard/ProductCard.tsx
+++ src/shared/ui/ProductCard/ProductCard.tsx
@@ -1,17 +1,31 @@
 import { useState, type FC } from 'react'
 import { ProductCounter } from '../ProductCounter'
+import { Heart } from 'lucide-react'
 
 interface IProductCardProps {
   title: string
   price: string
+  inFavorites: boolean
   status: 'in-cart' | 'not-in-cart' | 'out-of-stock'
 }
 
-export const ProductCard: FC<IProductCardProps> = ({ title, price, status }) => {
+export const ProductCard: FC<IProductCardProps> = ({ title, price, inFavorites, status }) => {
   const [cardStatus, setCardStatus] = useState(status)
+  const [favorites, setFavorites] = useState(inFavorites)
 
   return (
-    <div className='col-span-3 flex flex-col justify-between h-550px'>
+    <div className='col-span-3 flex flex-col justify-between h-550px relative'>
+      <button
+        className='absolute top-20px right-20px cursor-pointer'
+        onClick={() => setFavorites(!favorites)}
+      >
+        <Heart
+          size={24}
+          fill={favorites ? '#2B0C1A' : 'none'}
+          color={favorites ? 'none' : '#2B0C1A'}
+          strokeWidth={1.5}
+        />
+      </button>
       <img src='' alt='' className='w-full h-430px' />
       <h3>{title}</h3>
       {cardStatus === 'out-of-stock' ? (
