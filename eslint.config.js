3515ec7c67d01574a6cff5d08e37f4012c055395
Viсtoria <manaichevavika@gmail.com>
2026-03-04 00:05:41 +0300

    feature: верстка ContacsPage (#7)
    * feat: сделана верстка основных блоков
    
    * feat: добавлена вспомогательная функция cn
    
    * fix: разделила страницу на компоненты, улучшила стили
    
    * fix: исправлены типы, карта вынесена в отдельный компонент
    
    * feature: верстка SubHeader (#5)
    
    * feat: сделана верстка компонента
    
    * fix: добавлена функциональность в компонент (параметры, props)
    
    * chore: добавлены обновления конфигураций в git
    
    * fix: скорректирована верстка, исправлены типы, декомпозирован компонент
    
    * fix: добавлен SubHeader, исправлен формат картинок, добавлено несколько переменных для размеров
    
    * fix: иконки обратно заменены на SVG, убраны комментарии с вопросами

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,31 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import reactX from "eslint-plugin-react-x";
-import reactDom from "eslint-plugin-react-dom";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommendedTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      reactX.configs["recommended-typescript"],
+      reactX.configs['recommended-typescript'],
       reactDom.configs.recommended,
     ],
     languageOptions: {
       parserOptions: {
-        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
         tsconfigRootDir: import.meta.dirname,
       },
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-]);
+])
