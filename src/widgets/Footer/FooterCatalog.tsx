5015411638b6a87a11961d6866040c307c87cc12
Vika Manaicheva <manaichevavika@gmail.com>
2026-03-04 17:53:59 +0300

    fix(footer): исправил наезд нижней границы (Дима)

--- src/widgets/Footer/FooterCatalog.tsx
+++ src/widgets/Footer/FooterCatalog.tsx
@@ -1,66 +1,66 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 export function FooterCatalog() {
   return (
     <div>
-      <h3 className="text-footer-title">Каталог</h3>
-      <ul className="text-footer-list">
+      <h3 className='text-footer-title'>Каталог</h3>
+      <ul className='text-footer-list'>
         <li>
-          <Link to="/catalog/lustry" className="hover:text-white">
+          <Link to='/catalog/lustry' className='hover:text-white'>
             Люстры
           </Link>
         </li>
         <li>
-          <Link to="/catalog/svetilniki" className="hover:text-white">
+          <Link to='/catalog/svetilniki' className='hover:text-white'>
             Светильники
           </Link>
         </li>
         <li>
-          <Link to="/catalog/tochnevye-svetilniki" className="hover:text-white">
+          <Link to='/catalog/tochnevye-svetilniki' className='hover:text-white'>
             Точечные светильники
           </Link>
         </li>
         <li>
-          <Link to="/catalog/bra" className="hover:text-white">
+          <Link to='/catalog/bra' className='hover:text-white'>
             Бра
           </Link>
         </li>
         <li>
-          <Link to="/catalog/podsvetki" className="hover:text-white">
+          <Link to='/catalog/podsvetki' className='hover:text-white'>
             Подсветки
           </Link>
         </li>
         <li>
-          <Link to="/catalog/nastolnye-lampy" className="hover:text-white">
+          <Link to='/catalog/nastolnye-lampy' className='hover:text-white'>
             Настольные лампы
           </Link>
         </li>
         <li>
-          <Link to="/catalog/torshery" className="hover:text-white">
+          <Link to='/catalog/torshery' className='hover:text-white'>
             Торшеры
           </Link>
         </li>
         <li>
-          <Link to="/catalog/trekovye-sistemy" className="hover:text-white">
+          <Link to='/catalog/trekovye-sistemy' className='hover:text-white'>
             Трековые системы
           </Link>
         </li>
         <li>
-          <Link to="/catalog/nochniki" className="hover:text-white">
+          <Link to='/catalog/nochniki' className='hover:text-white'>
             Ночники
           </Link>
         </li>
         <li>
-          <Link to="/catalog/ulichnye-svetilniki" className="hover:text-white">
+          <Link to='/catalog/ulichnye-svetilniki' className='hover:text-white'>
             Уличные светильники
           </Link>
         </li>
         <li>
-          <Link to="/catalog/komplektuyushchie" className="hover:text-white">
+          <Link to='/catalog/komplektuyushchie' className='hover:text-white'>
             Комплектующие
           </Link>
         </li>
       </ul>
     </div>
-  );
+  )
 }
--- src/widgets/Footer/FooterContacts.tsx
+++ src/widgets/Footer/FooterContacts.tsx
@@ -8,7 +8,7 @@ export function FooterContacts() {
         <p className='text-footer-list text-2xl'>imperiya_lustr@mail.ru</p>
       </div>
 
-      <div className='flex items-center gap-10 mt-2'>
+      <div className='flex justify-end items-center gap-10 mt-2'>
         <img src='/icons/social/max-brown.svg' className='w-5 h-5 cursor-pointer' alt='Max' />
         <img
           src='/icons/social/telegram-brown.svg'
