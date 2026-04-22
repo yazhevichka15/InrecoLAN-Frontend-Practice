1d0702c212db5d6357bfa6586533d2267371c9d8
IrinaD7 <irinadimakova05@yandex.ru>
2026-04-22 11:55:15 +0300

    feat: добавлена страница избранного

---
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -0,0 +1,31 @@
+import { SubHeader } from '@widgets/SubHeader'
+import { getProducts } from '@pages/CategoryPage/api/getProducts'
+import { ProductCard } from '@shared/ui/ProductCard'
+
+const chandeliersItems = getProducts()
+
+export const FavoritesPage = () => {
+  return (
+    <>
+      <SubHeader
+        mainTitle='Избранные'
+        subTitle='товары'
+        description='Поможем подобрать люстру под ваш интерьер, чтобы в доме было светло, уютно и комфортно каждый день.'
+        productCount={chandeliersItems.length}
+      />
+      <div className='gap-base p-(--basic-container) w-full grid grid-cols-12'>
+        {chandeliersItems.map((card) => (
+          <ProductCard
+            key={card.title}
+            image={card.image}
+            title={card.title}
+            price={card.price}
+            status={card.status}
+            badge={card.badge}
+            oldPrice={card.oldPrice}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
